c4d44d5a28a1afa2048822312b0ffe4c6b5584b3
bang-star <bangseongmin@naver.com>
2023-03-03 00:07:08 +0900

    #3 - [2일차]  컴포넌트 사용 방법
    컴포넌트를 분리하여 재사용을 높이는 방법을 알아보았고
    간단하게 Props를 사용하여 데이터를 전달하는 방법을 확인할 수 있었다.

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,24 +1,17 @@
 <template>
     <div>
-        <h1>{{ title }}</h1>
-        <h2>{{ message }}</h2>
-        <button v-on:click="reverseMessage">메시지 뒤집기</button>
-        <button @click="reverseMessage">메시지 뒤집기</button>
-
-        <hr>
-        <h2>{{ message2 }}</h2>
-        <input type="text" v-model="message2">
+        <h4>{{ todo.name }}</h4>
     </div>
 </template>
 
 <script>
 export default {
     name: 'HelloWorld',
+    props: ['todo'],
     data() {
         return {
             title: 'Vue World',
             message: '안녕하세요! Daniel입니다.',
-            message2: '안녕하세요! Daniel입니다.',
         }
     },
     watch: {
