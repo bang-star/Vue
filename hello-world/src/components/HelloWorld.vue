ee9b61f274d234dd63240727e04f7ae4f9ec4b22
bang-star <bangseongmin@naver.com>
2023-03-03 01:38:51 +0900

    #7 - 데이터와 메서드
    1. Object.freeze 메서드
    2. data 에 있는 속성들은 인스턴스가 생성될 때 존재한 것들만 반응형
    3. Vue 인스턴스는 데이터 속성 이외에도 유용한 인스턴스 속성 및 메서드를 제공한다. 다른 사용자 정의 속성과 구분하기 위해 $ 접두어를 붙인다.

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,17 +1,32 @@
 <template>
     <div>
-        <h4>{{ todo.name }}</h4>
+        <!-- <h1>{{ message }}</h1> -->
+        <h3>{{ test.a }}</h3>
+        <button @click="test.a = '2'">test 버튼</button>
+        <h3>{{ testConst.foo }}</h3>
+        <button @click="testConst.foo = 'foo'">testConst 버튼</button>
     </div>
 </template>
 
 <script>
+let test = {
+    a : 1,
+}
+
+let testConst = {
+    foo: 'bar',
+}
+
+Object.freeze(testConst)
+
 export default {
     name: 'HelloWorld',
-    props: ['todo'],
+    // props: ['todo'],
     data() {
         return {
-            title: 'Vue World',
             message: '안녕하세요! Daniel입니다.',
+            test: test,
+            testConst: testConst
         }
     },
     watch: {
