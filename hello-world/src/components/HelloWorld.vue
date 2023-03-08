3514cd6ef2320c1f283924f127dbb92ba5aa914d
bang-star <bangseongmin@naver.com>
2023-03-09 00:26:52 +0900

    #18 - Watch 속성
    Vue 2.0 API 문서를 보며 Watch 속성 학습

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,42 +1,57 @@
 <template>
     <div>
-        <h2>Hello, Vue World</h2>
-        <h2 style="color: blue">이름 : {{ firstName }}</h2>
-        <h2 style="color: red">성: {{ lastName }}</h2>
-        <h2 style="color: green">성명: {{ fullName }}</h2>
+        <h1>Welcome to Vue World</h1>
 
-        <button @click="fullNameGS = 'bang star'">Setter button</button>
+        <p>
+            Ask a yes/no question:
+            <input v-model="question">
+        </p>
+        <p>{{ answer }}</p>
     </div>
 </template>
 
+
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
 <script>
-export default {
+export default{
     name: 'HelloWorld',
     data() {
-        return{
-            firstName: 'Daniel',
-            lastName: 'Bang',
+        return {
+                question: '',
+                answer: 'I cannot give you an answer until you ask a question!'
+            }
+    },
+    watch: {
+        question: function (newQuestion, oldQuestion) {
+            this.answer = 'Waiting for you to stop typing...'
+            this.debouncedGetAnswer()
         }
     },
-    computed: {
-       fullName() {
-            return this.firstName + ' ' + this.lastName
-       },
-       fullNameGS: {
-            // getter
-            get: function() {
-                return this.firstName + ' ' + this.lastName
-            },
-            set: function(newVal) {
-                const names = newVal.split(' ')
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
-            }
-       }
+    created: function () {
+        // _.debounce is a function provided by lodash to limit how
+        // often a particularly expensive operation can be run.
+        // In this case, we want to limit how often we access
+        // yesno.wtf/api, waiting until the user has completely
+        // finished typing before making the ajax request. To learn
+        // more about the _.debounce function (and its cousin
+        // _.throttle), visit: https://lodash.com/docs#debounce
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
     },
     methods: {
-        reversedmessage2: function(){
-            return this.message.split('').reverse().join('')
+        getAnswer: function () {
+        if (this.question.indexOf('?') === -1) {
+            this.answer = 'Questions usually contain a question mark. ;-)'
+            return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+            .then(function (response) {
+                vm.answer = _.capitalize(response.data.answer)
+            })
+            .catch(function (error) {
+                vm.answer = 'Error! Could not reach the API. ' + error
+            })
         }
     }
 }
