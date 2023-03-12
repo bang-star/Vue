97b52b5484acb105afb02ae652c63ac4585195c6
bang-star <bangseongmin@naver.com>
2023-03-13 02:09:37 +0900

    # 28 - HTML 클래스 바인딩하기(컴포넌트와 함께 사용하는 방법)

---
+++ hello-world/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="foo bar">
+        <h2>HiHiHi</h2>
+    </div>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+        }
+    },
+}
+</script>
