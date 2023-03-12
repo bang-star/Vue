dd72ba6145620b94d18d3694fafd86b6d6ae06c8
bang-star <bangseongmin@naver.com>
2023-03-13 02:18:52 +0900

    #28 - 인라인 스타일 바인딩하기(객체 구문)

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,41 +1,30 @@
 <template>
     <div>
         <h1>Welcome to Vue World</h1>
-        <my-component class="baz boo"></my-component>
-        <my-component :class="{active: isActive}"></my-component>
+        <!-- 인라인 -->
+        <div v-bind:style="{ color: activeColor, fontSize: fontSize + 'px' }">South</div>
+        <!-- 객체 -->
+        <div v-bind:style="styleObject">Korea</div>
     </div>
 </template>
 
 <script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
 
 <script>
-import MyComponent from "./MyComponent.vue"
-
 export default {
-    components: {
-        MyComponent,
-    },
     name: 'HelloWorld',
     data() {
         return {
-            isActive: true,
-            hasError: false,
-            error: null,
-            classObject: {
-                active: true,
-                'text-danger': false
-            },
-            activeClass: 'active',
-            errorClass: 'text-danger'
+            activeColor: 'red',
+            fontSize: '30',
+            styleObject: {
+                color: 'red',
+                fontSize: '20px',
+            }
         }
     },
     computed: {
-        classObject2() {
-            return {
-                active: this.isActive && !this.error,
-                'text-danger': this.error && this.error.type === 'fatal'
-            }
-        }
+
     }
 }
 </script>
