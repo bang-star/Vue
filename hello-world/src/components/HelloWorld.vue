1099a29c840468901272014f2716338371c4ff78
bang-star <bangseongmin@naver.com>
2023-03-16 07:41:28 +0900

    #29 - v-show
    v-if는 주석되어있는데, v-show는 css적으로 display=none처리된다.

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,16 +1,10 @@
 <template>
     <div>
         <h1>Welcome to Vue World</h1>
-        <h2 v-if="awesome">Awesome</h2>
-        <h2 v-else>Nope</h2>
-        <template v-if="loginType === 'username'">
-            <label>사용자 이름: </label>
-            <input type="text" placeholder="사용자 이름을 입력하세요." key="username-input">
-        </template>
-        <template v-else>
-            <label>이메일: </label>
-            <input type="text" placeholder="이메일 주소를 입력하세요." key="email-input">
-        </template>
+
+        <div v-show="isShow">
+            I'm Daniel. It is div for `v-show`
+        </div>
         <div>
             <button @click="toggle">Toggle</button>
         </div>
@@ -24,14 +18,12 @@ export default {
     name: 'HelloWorld',
     data() {
         return {
-            awesome: true,
-            loginType: 'username',
+            isShow: true,
         }
     },
     methods: {
         toggle() {
-            if(this.loginType === 'username') this.loginType = 'email'
-            else this.loginType = 'username'
+            this.isShow = !isShow
         }
     }
 }
