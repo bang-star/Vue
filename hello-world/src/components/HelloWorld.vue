d4bd6b850a10432b0d4b9593e9b39f8ecda6c66a
bang-star <bangseongmin@naver.com>
2023-03-20 02:31:45 +0900

    init

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,27 +1,6 @@
 <template>
     <div>
         <h1>Welcome to Vue World</h1>
-        <ul id='example-1'>
-            <li 
-                v-for="todo in todos" 
-                v-bind:key="todo.id">
-                <span v-if="todo.isComplete">{{ todo.title }}</span>
-            </li>
-        </ul>
-        <ul id='example-2'>
-            <li 
-                v-for="todo in completedTodos" 
-                v-bind:key="todo.id">
-                <span>{{ todo.title }}</span>
-            <li v-for="(value, name) in object" v-bind:key="value">
-                {{ value }} : {{ name }}
-            </li>
-        </ul>
-        <ul id='example-2'>
-            <li v-for="(value, name, index) in object" v-bind:key="value">
-                {{ value }} : {{ name }} : {{ index }}
-            </li>
-        </ul>
     </div>
 </template>
 
@@ -31,15 +10,7 @@
 export default {
     name: 'HelloWorld',
     data() {
-        return {
-            todos: [
-                { id : 1, title: '제목 1', isComplete: false},
-                { id : 2, title: '제목 2', isComplete: true},
-                { id : 3, title: '제목 3', isComplete: true},
-                { id : 4, title: '제목 4', isComplete: false},
-                { id : 5, title: '제목 5', isComplete: true},
-            ]
-        }
+       
     },
     methods: {
     }
