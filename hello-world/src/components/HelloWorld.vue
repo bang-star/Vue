f8cef9b15415cf6bb2aabf635fab733befbea1a6
bang-star <bangseongmin@naver.com>
2023-03-22 00:45:20 +0900

    #33 - [컴포넌트] 컴포넌트 사용하기 - 지역 등록

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <MyComponent></MyComponent>
+        <my-component></my-component>
     </div>
 </template>
 
 <script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
 
 <script>
+import MyComponent from './MyComponent.vue'
+
 export default {
+    components: {
+        'my-component' : MyComponent
+    },
     name: 'HelloWorld',
     data() {
        return {
