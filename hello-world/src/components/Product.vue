7cad7c541053c421a326d935829d9a909de74269
bang-star <63120360+bang-star@users.noreply.github.com>
2023-05-21 18:20:26 +0900

    #82 [Vue-Router-Advanced]: 네비게이션 가드 (#122)

--- hello-world/src/components/Product.vue
+++ hello-world/src/components/Product.vue
@@ -15,7 +15,7 @@ export default ({
     },
     data() {
         return {
-
+            message: 'hello vue-world'
         }
     },
     computed: {
@@ -23,6 +23,17 @@ export default ({
     },
     methods: {
 
+    },
+    beforeRouterEnter (to, from, next) {
+        // 이 컴포넌트를 렌더링하는 라우트 앞에 호출됩니다.
+        // 이 가드가 호출 될 때 아직 생성되지 않았기 때문에
+        // `this` 컴포넌트 인스턴스에 접근할 수 없습니다.
+        console.log("beforeRouterEnter" + this.message)
+    },
+    beforeRouteLeave (to, from, next) {
+        // 이 컴포넌트를 렌더링하는 라우트가 이전으로 네비에기션 될 때 호출됩니다.
+        // `this` 컴포넌트 인스턴스에 접근 할 수 있습니다.
+        console.log("beforeRouteLeave" + this.message)
     }
 })
 </script>
