ba76d1eddeddc773e3861bddb350139425a58097
daniel <daniel@mobidoc.us>
2023-05-21 15:51:40 +0900

    #71 [Vue-Router-Basic] 동적 라우트 매칭

--- hello-world/src/router/route.js
+++ hello-world/src/router/route.js
@@ -1,9 +1,11 @@
 import Foo from '../components/Foo.vue'
 import Bar from '../components/Bar.vue'
+import Product from '../components/Product.vue'
 
 const Routes = [
     { path: '/foo', component: Foo },
     { path: '/bar', component: Bar },
+    { path: '/product/:id', component: Product },
 ]
 
 export default Routes
