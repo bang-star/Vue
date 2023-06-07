99beed3a0cd71fc66914b70c183a85b49d901ef1
bang-star <bangseongmin@naver.com>
2023-06-08 01:56:47 +0900

    #88 [Vuex-Basic]: Vuex

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import Router from "./router/index"
+// import Vuex from 'vuex'
+import { store } from './store';
 
 // production setting
 Vue.config.productionTip = false
+// Vue.use(Vuex)
 
 // Vue 인스턴스 생성
 
 const app = new Vue({
+  store: store,
   router: Router,
   render: h => h(App),
 })
--- hello-world/src/router/route.js
+++ hello-world/src/router/route.js
@@ -1,13 +1,15 @@
-import Foo from '../components/Foo.vue'
-import Bar from '../components/Bar.vue'
-import Product from '../components/Product.vue'
+// import Foo from '../components/Foo.vue'
+// import Bar from '../components/Bar.vue'
+// import Product from '../components/Product.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 
 const Routes = [
-    { path: '/foo', name: 'foo', component: Foo, alias: '/foofoo' },
-    { path: '/bar', name: 'bar', component: Bar },
-    { path: '/product/:id', name: 'product', props: {news: false}, component: Product, beforeEnter: (to, from) => {
-        console.log(to, from)
-    } },
+    // { path: '/foo', name: 'foo', component: Foo, alias: '/foofoo' },
+    // { path: '/bar', name: 'bar', component: Bar },
+    // { path: '/product/:id', name: 'product', props: {news: false}, component: Product, beforeEnter: (to, from) => {
+    //     console.log(to, from)
+    // } },
+    { path: '*', component: HelloWorld }
 ]
 
 export default Routes
